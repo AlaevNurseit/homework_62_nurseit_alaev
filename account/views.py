031565a77ad00600c6b51d2710c679cda83570b8
Nurseit <alaevnurik312@gmail.com>
2026-06-29 13:36:26 +0600

    добавил представление для регистрации

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
+from account.forms import MyUserCreationForm
+
 
 def login_view(request):
     context = {}
@@ -10,7 +12,8 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('issues:project_list')
+            next_url = request.GET.get('next')
+            return redirect(next_url if next_url else 'issues:project_list')
         else:
             context['has_error'] = True
     return render(request, 'account/login.html', context=context)
@@ -19,6 +22,18 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('account:login')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            next_url = request.GET.get('next')
+            return redirect(next_url if next_url else 'issues:project_list')
+    else:
+        form = MyUserCreationForm()
+    return render(request, 'account/register.html', context={'form': form})
 
 
 
